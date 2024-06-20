129f4b9769be909afc934f94d3c458d1831cd237
Martin Dutria <martinmdutria@gmail.com>
2024-06-20 02:27:04 -0300

    basic post request function added

--- extensions/recibopago-btn/src/Checkout.jsx
+++ extensions/recibopago-btn/src/Checkout.jsx
@@ -4,25 +4,38 @@ import {
   Button,
   useApi,
 } from '@shopify/ui-extensions-react/checkout';
+import React from 'react';
 export default reactExtension(
   'purchase.thank-you.block.render',
   () => <Extension />,
 );
 
+function PostRequest(param) {
+  fetch('https://6673b1d675872d0e0a9349af.mockapi.io/api/shopify', {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(param)
+  }).then(() => {
+    console.log('data sent');
+  })
+}
+
+
 
 function Extension() {
   const api = useApi()
   var data = {"amount": api.cost.totalAmount.current.amount,
-    "customer": api.buyerIdentity.customer.current,
+    "currency": api.cost.totalAmount.current.currencyCode,
     "email": api.buyerIdentity.email.current,
     "order-id": api.orderConfirmation.current.order.id
-
   }
+
+
   return (
     <InlineStack>
       <Button
         onPress={() => {
-          console.log(data)
+          PostRequest(data)
         }}
       >
         Pagar con ReciboPagos
